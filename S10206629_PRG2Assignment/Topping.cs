dee3920adc0264cbd8f18bcf4a65ed23f0972560
emmanuelrafol@gmail.com <emmanuelrafol@gmail.com>
2024-01-09 13:55:44 +0800

    start of the assignment

---
+++ S10206629_PRG2Assignment/Topping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10206629_PRG2Assignment
+{
+    internal class Topping
+    {
+        public string type {  get; set; }
+        Topping() { }
+        Topping(string Type) 
+        {
+            this.type = Type;
+        }
+    }
+}
