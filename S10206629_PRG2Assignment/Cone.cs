b736b218049d0f74ac2e514550e3d5ffc1ee01ab
emmanuelrafol@gmail.com <emmanuelrafol@gmail.com>
2024-01-09 14:58:22 +0800

    added Waffle and Cone class

---
+++ S10206629_PRG2Assignment/Cone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10206629_PRG2Assignment
+{
+    internal class Cone : IceCream
+    {
+        public bool Dipped {  get; set; }  
+        public Cone() { }
+        public Cone(string option, int scoops, List<Flavour> flavour, List<Topping> topping, bool dipped) 
+        {
+            this.Option = option;
+            this.Scoops = scoops;
+            this.Flavours = flavour;
+            this.Toppings = topping;
+            this.Dipped = dipped;
+        }
+
+        public override double CalculatePrice()
+        {
+            double price = 0;
+            // scoops price
+            if (this.Scoops == 1)
+            {
+                price = 4.00;
+            }
+            else if (this.Scoops == 2) 
+            {
+                price = 5.50;
+            }
+            else
+            {
+                price = 6.50;
+            }
+
+            // flavour price
+            foreach (Flavour flavour in Flavours)
+            {
+                if (flavour.Premium = true)
+                {
+                    price += 2;
+                }
+            }
+
+            //toppings
+            price += this.Toppings.Count();
+
+            //dipped
+            if (this.Dipped) { price += 2; }
+
+            return price;
+        }
+    }
+}
---
+++ S10206629_PRG2Assignment/Waffle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10206629_PRG2Assignment
+{
+    internal class Waffle : IceCream
+    {
+        public string WaffleFlavour {  get; set; }  
+        public Waffle() { }
+        public Waffle(string option, int scoops, List<Flavour> flavour, List<Topping> topping, string waffleflavour) 
+        {
+            this.Option = option;
+            this.Scoops = scoops;
+            this.Flavours = flavour;
+            this.Toppings = topping;
+            this.WaffleFlavour = waffleflavour;
+        }
+
+        public override double CalculatePrice()
+        {
+            double price = 0;
+            // scoops price
+            if (this.Scoops == 1)
+            {
+                price = 7.00;
+            }
+            else if (this.Scoops == 2) 
+            {
+                price = 8.50;
+            }
+            else
+            {
+                price = 9.50;
+            }
+
+            // flavour price
+            foreach (Flavour flavour in Flavours)
+            {
+                if (flavour.Premium = true)
+                {
+                    price += 2;
+                }
+            }
+
+            //toppings
+            price += this.Toppings.Count();
+
+            //waffle flavour
+            if (this.WaffleFlavour == "Red velvet" || this.WaffleFlavour ==  "charcoal" || this.WaffleFlavour == "pandan") { price += 3; }
+
+            return price;
+        }
+    }
+}
