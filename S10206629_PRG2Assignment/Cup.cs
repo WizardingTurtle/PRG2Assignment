48d1cc47c7d1a41b15fc37dc9b22f7f674373e0d
emmanuelrafol@gmail.com <emmanuelrafol@gmail.com>
2024-01-09 14:47:13 +0800

    added Ice Cream and Cup classes

---
+++ S10206629_PRG2Assignment/Cup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10206629_PRG2Assignment
+{
+    internal class Cup : IceCream
+    {
+        public Cup() { }
+        public Cup(string option, int scoops, List<Flavour> flavour, List<Topping> topping) 
+        {
+            this.Option = option;
+            this.Scoops = scoops;
+            this.Flavours = flavour;
+            this.Toppings = topping;
+        }
+
+        public override double CalculatePrice()
+        {
+            double price = 0;
+            // scoops price
+            if (this.Scoops == 1)
+            {
+                price = 4.00;
+            }
+            else if (this.Scoops == 2) 
+            {
+                price = 5.50;
+            }
+            else
+            {
+                price = 6.50;
+            }
+
+            // flavour price
+            foreach (Flavour flavour in Flavours)
+            {
+                if (flavour.Premium = true)
+                {
+                    price += 2;
+                }
+            }
+
+            //toppings
+            price += this.Toppings.Count();
+
+            return price;
+        }
+    }
+}
---
+++ S10206629_PRG2Assignment/IceCream.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10206629_PRG2Assignment
+{
+    internal abstract class IceCream
+    {
+        public string Option {  get; set; }
+        public int Scoops { get; set; }
+        public List<Flavour> Flavours { get; set; } = new List<Flavour>(new Flavour[4]);
+        public List<Topping> Toppings { get; set; } = new List<Topping>(new Topping[3]);
+
+        public IceCream() { }
+        public IceCream(string option, int scoops, List<Flavour> flavours, List<Topping> toppings)
+        {
+            this.Option = option;
+            this.Scoops = scoops;
+            this.Flavours = flavours;
+            this.Toppings = toppings;
+        }
+        public abstract double CalculatePrice();
+        public override string ToString()
+        {
+            return Option + Scoops + Flavours + Toppings;
+        }
+    }
+}
