273a2620f92aa74ba6550016f489d613a22f0aff
Jun Kai <148022731+3xothicc@users.noreply.github.com>
2024-01-09 15:12:38 +0800

    Add files via upload

---
+++ S10206629_PRG2Assignment/Customer.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class Customer
+{
+    public string Name;
+    public int MemberId;
+    public DateTime Dob;
+    public Order CurrentOrder;
+    private List<Order> OrderHistory;
+    public PointCard Rewards;
+
+    public Customer()
+    {
+        OrderHistory = new List<Order>();
+        Rewards = new PointCard();
+    }
+
+    public Customer(string name, int memberId, DateTime dob)
+    {
+        Name = name;
+        MemberId = memberId;
+        Dob = dob;
+        OrderHistory = new List<Order>();
+        Rewards = new PointCard();
+    }
+
+
+    public Order MakeOrder()
+    {
+        CurrentOrder = new Order();
+        return CurrentOrder;
+    }
+
+    public bool IsBirthday()
+    {
+        return DateTime.Today.Month == Dob.Month && DateTime.Today.Day == Dob.Day;
+    }
+
+    public override string ToString()
+    {
+        return $"Customer: {Name}, Member ID: {MemberId}, DOB: {Dob.ToShortDateString()}, Rewards Points: {Rewards.Points}";
+    }
+}
---
+++ S10206629_PRG2Assignment/PointCard.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+
+public class PointCard
+{
+    public int Points;
+    public int PunchCard;
+    public string Tier;
+
+    // Constructors
+    public PointCard()
+    {
+        
+    }
+
+    public PointCard(int points, int punchCard)
+    {
+        Points = points;
+        PunchCard = punchCard;
+    }
+
+    public void AddPoints(int pointsToAdd)
+    {
+        Points += pointsToAdd;
+    }
+
+    public void RedeemPoints(int pointsToRedeem)
+    {
+        if (pointsToRedeem <= Points)
+        {
+            Points -= pointsToRedeem;
+        }
+        else
+        {
+            Console.WriteLine("Not enough points to redeem.");
+        }
+    }
+
+    public void Punch()
+    {
+    }
+
+    public override string ToString()
+    {
+        return $"Points: {Points}, PunchCard: {PunchCard}, Tier: {Tier}";
+    }
+}
+
