3893d3fc2a40c9adb6b8eb2d3d9d176309e0f24a
emmanuelrafol@gmail.com <emmanuelrafol@gmail.com>
2024-01-15 11:37:00 +0800

    updated Program.CS to hold place holder code

--- S10206629_PRG2Assignment/Program.cs
+++ S10206629_PRG2Assignment/Program.cs
@@ -4,11 +4,64 @@
 // Partner Name : Goh Jun Kai
 //==========================================================
 
+// To add code to create customers and orders from data files
 
+//Main Program Loop
+void MenuStart()
+{
+    Console.WriteLine
+        (
+        "-----      I.C.Treats Menu     -----\r\n" +
+        "[1] List all customers\r\n" +
+        "[2] List all current orders\r\n" +
+        "[3] Register a new customer\r\n" +
+        "[4] Create a customer's order\r\n" +
+        "[5] Display order details of customer\r\n" +
+        "[6] Modify order details\r\n" +
+        "[0] Exit Program\r\n" +
+        "------------------------------------"
+        );
+    Console.Write("Please Choose an option");
+    String option = Console.ReadLine(); 
+    
+    if ( option = '1' )
+    {
+        //Placeholder
+    }
+    else if (option = '2')
+    {
+        //Placeholder
+    }
+    else if (option = '3')
+    {
+        //Placeholder
+    }
+    else if (option = '4')
+    {
+        //Placeholder
+    }
+    else if (option = '5')
+    {
+        //Placeholder
+    }
+    else if (option = '6')
+    {
+        //Placeholder
+    }
+    else if (option = '0')
+    {
+        Console.WriteLine("Closing the program - bye bye!");
+        break;
+    }
 
+//Feature 1 List all customers
 
-//Feature 2
+//Feature 2 List all current orders
 
-//Feature 5
+//Feature 3 Register a new customer
 
-//Feature 6
+//Feature 4 Create a customer’s order
+
+//Feature 5 Display order details of a customer
+
+//Feature 6 ) Modify order details
